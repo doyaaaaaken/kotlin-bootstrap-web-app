426a2c21d5042b1def2aff528772601e79ea4de5
doyaaaaaken <kk03140816@gmail.com>
2018-03-18 20:10:49 +0900

    Connect to postgresql.

--- src/main/kotlin/org/doyaaaaaken/HomeController.kt
+++ src/main/kotlin/org/doyaaaaaken/HomeController.kt
@@ -1,5 +1,7 @@
 package org.doyaaaaaken
 
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Table
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,5 +16,19 @@ class HomeController {
     fun index(@RequestParam(value = "name", defaultValue = "World") name: String, model: Model): String {
         model.addAllAttributes(mapOf("test" to "testValue"))
         return "home/show"
+    }
+
+    @GetMapping("test")
+    fun test(@RequestParam(value = "name", defaultValue = "World") name: String, model: Model): String {
+        //TODO: driverの設定を、設定ファイルに移す
+        Database.connect("jdbc:postgresql://localhost:5442/dykn",  org.postgresql.Driver::class.java.name)
+
+        model.addAllAttributes(mapOf("test" to "testValue"))
+        return "home/show"
+    }
+
+    object Users: Table("users") {
+        val id = long("id").primaryKey()
+        val name = varchar("name", 32)
     }
 }
