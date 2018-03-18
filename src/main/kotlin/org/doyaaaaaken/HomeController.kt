75f30191d0dd4c3e082a19fea039d923b0fc7304
doyaaaaaken <kk03140816@gmail.com>
2018-03-18 20:22:26 +0900

    Add sample code of exposed dsl.

--- src/main/kotlin/org/doyaaaaaken/HomeController.kt
+++ src/main/kotlin/org/doyaaaaaken/HomeController.kt
@@ -2,11 +2,15 @@ package org.doyaaaaaken
 
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import java.time.LocalDateTime
 
 @Controller
 @RequestMapping("/")
@@ -19,16 +23,26 @@ class HomeController {
     }
 
     @GetMapping("test")
-    fun test(@RequestParam(value = "name", defaultValue = "World") name: String, model: Model): String {
+    fun test(model: Model): String {
         //TODO: driverの設定を、設定ファイルに移す
-        Database.connect("jdbc:postgresql://localhost:5442/dykn",  org.postgresql.Driver::class.java.name)
+        Database.connect("jdbc:postgresql://localhost:5442/dykn",  org.postgresql.Driver::class.java.name, "dykn")
+
+        transaction {
+            Users.insert {
+                it[name] = "name ${LocalDateTime.now()}"
+            }
+
+            Users.selectAll().forEach {
+                println("Result:   ${it[Users.name]}")
+            }
+        }
 
         model.addAllAttributes(mapOf("test" to "testValue"))
         return "home/show"
     }
 
     object Users: Table("users") {
-        val id = long("id").primaryKey()
+        val id = long("id").primaryKey().autoIncrement()
         val name = varchar("name", 32)
     }
 }
