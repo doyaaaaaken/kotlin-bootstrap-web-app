59eba7744b2be9eeef0cb074d5e7d7989f126531
doyaaaaaken <kk03140816@gmail.com>
2018-03-18 14:54:49 +0900

    Create freemarker sample.

--- src/main/kotlin/org/doyaaaaaken/HomeController.kt
+++ src/main/kotlin/org/doyaaaaaken/HomeController.kt
@@ -1,17 +1,19 @@
 package org.doyaaaaaken
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
-@RestController
+@Controller
 class HomeController {
 
     val counter = AtomicLong()
 
     @GetMapping("/")
     fun index(@RequestParam(value = "name", defaultValue = "World") name: String): String {
-        return "Hello ${counter.incrementAndGet()} $name"
+//        return "Hello ${counter.incrementAndGet()} $name"
+        return "show"
     }
 }
